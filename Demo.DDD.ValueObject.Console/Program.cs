786f918acba13bf6aa0d358bffe71fd03ae7c3f8
Admir Mujkic <admir.mujkic@applicita.com>
2021-07-30 11:12:08 +0200

    Added Demo.DDD.ValueObject solution

---
+++ Demo.DDD.ValueObject.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.DDD.ValueObject.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
